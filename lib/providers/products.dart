4390d89ab0ef47de86d1631f680499b15b9ab5d3
Kevin Cruz <kocruz.24@gmail.com>
2021-10-19 15:01:01 -0400

    [200] Local State vs App-wide State

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -6,13 +6,33 @@ import 'product.dart';
 class Products with ChangeNotifier {
   List<Product> _items = [...products_data];
 
+  var _showFavoritesOnly = false;
+
   List<Product> get items {
+    // if (_showFavoritesOnly) {
+    //   return _items.where((prodItem) => prodItem.isFavorite).toList();
+    // }
+
     return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
 
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
   }
+
+  // void showFavoritesOnly() {
+  //   _showFavoritesOnly = true;
+  //   notifyListeners();
+  // }
+
+  // void showAll() {
+  //   _showFavoritesOnly = false;
+  //   notifyListeners();
+  // }
 
   void addProduct() {
     // _items.add(value);
