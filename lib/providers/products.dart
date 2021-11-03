0a9cfcbae7d5d889638eff7870d5e362506ccb6d
Kevin Cruz <kocruz.24@gmail.com>
2021-11-03 01:13:05 -0400

    [232] Implement logic to update products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -48,4 +48,14 @@ class Products with ChangeNotifier {
 
     notifyListeners();
   }
+
+  void updateProduct(String id, Product newProduct) {
+    final productIndex = _items.indexWhere((prod) => prod.id == id);
+
+    if (productIndex >= 0) {
+      _items[productIndex] = newProduct;
+    }
+
+    notifyListeners();
+  }
 }
