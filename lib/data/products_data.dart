1dab5584b10e7e47b9159c4311058ca5e1174ebe
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-01 22:27:18 -0400

    Add more data to products_data.dart

--- lib/data/products_data.dart
+++ lib/data/products_data.dart
@@ -32,4 +32,29 @@ List<Product> products_data = [
     imageUrl:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
   ),
+  Product(
+    id: 'p5',
+    title: 'MacBook Pro 16"',
+    description: '"Apple M1 Max chip with 10-core CPU, 32-core GPU, and 16-core Neural Engine\n\n32GB unified memory\n\n1TB SSD storage\n\n16-inch Liquid Retina XDR display\n\nThree Thunderbolt 4 ports, HDMI port, SDXC card slot, MagSafe 3 port\n\n140W USB-C Power Adapter\n\nBakclit Magic Keyboard with Touch ID - US English"',
+    price: 3566.08,
+    isFavorite: true,
+    imageUrl:
+        'https://images.macrumors.com/t/jVH1zaP47QsCDufyEjaoCGm-oWo=/2769x/https://images.macrumors.com/article-new/2013/09/16-inch-macbook-pro.jpg',
+  ),
+  Product(
+    id: 'p6',
+    title: 'AirPods Max',
+    description: 'Very expensive Apple Headphones, but they are worth it, at least for me.',
+    price: 559.99,
+    imageUrl:
+        'https://www.etoren.com/upload/images/0.62387900_1619574159_apple-airpods-max-grey.jpg',
+  ),
+  Product(
+    id: 'p7',
+    title: 'MacBook Air 13',
+    description:'"M1 CPU\n8GB unified memory\n256GB SSD"',
+    price: 899.99,
+    imageUrl:
+        'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-gold-select-201810?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1633027804000.jpg',
+  )
 ];
